0e22b2e4bebc3caa5aaa23ddb7986874db6d8fbe
stefano <stef.pro@hotmail.com>
2023-12-31 04:36:46 -0500

    chore: add method to add a new guitar

--- helpers/guitar_api_functions.ts
+++ helpers/guitar_api_functions.ts
@@ -1,7 +1,31 @@
+import { Guitar } from "../types/guitar";
+
 class GuitarAPI {
     private static baseURL: string = "http://127.0.0.1:5000/v0"
 
     // ADD A GUITAR
+    static async addGuitar<T>(guitarData: Record<string, any>): Promise<T> {
+        try {
+            const response = await fetch(`${GuitarAPI.baseURL}/guitars/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(guitarData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Request failed with status ${response.status}`);
+            }
+
+            const data = await response.json();
+            return data as T;
+        } catch (error: any) {
+            console.error('API Request Error:', error.message);
+            throw error;
+        }
+    }
+
 
     // FETCH ALL GUITARS
     static async fetchGuitars<T>(): Promise<T> {
--- src/app/testForm/page.tsx
+++ src/app/testForm/page.tsx
@@ -5,16 +5,37 @@ import GuitarAPI from "../../../helpers/guitar_api_functions";
 
 const TestFormWithAuth = () => {
 
+    const dummyGuitar = {
+        "year": 2002,
+        "brand": "Fender",
+        "model": "Stratocaster American Deluxe",
+        "num_frets": 22,
+        "ss_frets": false,
+        "wood": {
+            "body": "mahogony",
+            "neck": "mahogony",
+            "fretboard": "mahogony"
+        },
+        "locking_tuners": false
+    }
+
     const handleButtonClick = () => {
         GuitarAPI.fetchGuitars()
             .then(result => console.log(result))
             .catch(error => console.error('Error fetching guitars:', error.message));
     };
 
+    const handleAddGuitar = () => {
+        GuitarAPI.addGuitar(dummyGuitar)
+            .then(result => console.log(result))
+            .catch(error => console.error('Error fetching guitars:', error.message));
+    }
+
     return (
         <AuthProvider>
             <TestForm />
             <button onClick={handleButtonClick}>Get all guitars</button>
+            <button onClick={handleAddGuitar}>Add a new guitar</button>
         </AuthProvider>);
 
 }
