cae9bc46b9ff7edecdf05e1f1fc9c4b3bd03323a
stefano <stef.pro@hotmail.com>
2023-12-31 20:56:25 -0500

    chore: create mobile nav bar and apply logic for showing appropriate nav bar

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,6 +1,9 @@
 'use client';
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Button, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material"
 import { makeStyles } from "@mui/styles";
+import { useState } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const useStyles = makeStyles(() => ({
     mainContainer: {
@@ -16,6 +19,11 @@ const useStyles = makeStyles(() => ({
         width: '85%',
         margin: 'auto',
     },
+    mobileNavBar: {
+        backgroundColor: 'white',
+        width: '100%',
+        margin: 'auto',
+    },
     navLinks: {
         width: '50%',
         display: 'flex',
@@ -25,6 +33,14 @@ const useStyles = makeStyles(() => ({
 }));
 
 const NavBar = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+    return isMobile ? <MobileNavBar /> : <DesktopNavBar />;
+}
+
+// ------------------------------------------------ Desktop App Bar --------------------------------------
+const DesktopNavBar = () => {
     const classes = useStyles()
 
     return (
@@ -47,6 +63,61 @@ const NavBar = () => {
                         </Typography>
                         <Button sx={{ borderRadius: '10px', padding: '10px 20px' }} disableElevation variant="contained">Register</Button>
                     </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+// ------------------------------------------------ Mobile Nav Bar -------------------------------------------
+const MobileNavBar = () => {
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+
+    const handleToggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <Box className={classes.mainContainer}>
+            <AppBar elevation={0} className={classes.navBar} position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ color: 'black', fontWeight: 'bold', flexGrow: 1, fontFamily: 'Montserrat, sans-serif' }}>
+                        Guitar Directory
+                    </Typography>
+                    <IconButton
+                        onClick={handleToggleDrawer}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Drawer PaperProps={{
+                        sx: { width: '100%' }
+                    }}
+                        anchor="left"
+                        open={open}
+                        onClose={handleToggleDrawer}>
+                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'end' }}>
+                            <IconButton
+                                onClick={handleToggleDrawer}
+                            >
+                                <CloseIcon />
+                            </IconButton>
+                        </Box>
+                        <List>
+                            <ListItem onClick={handleToggleDrawer}>
+                                <ListItemText primary="About" />
+                            </ListItem>
+                            <ListItem onClick={handleToggleDrawer}>
+                                <ListItemText primary="Contact" />
+                            </ListItem>
+                            <ListItem onClick={handleToggleDrawer}>
+                                <ListItemText primary="Login" />
+                            </ListItem>
+                            <ListItem onClick={handleToggleDrawer}>
+                                <ListItemText primary="Register" />
+                            </ListItem>
+                        </List>
+                    </Drawer>
                 </Toolbar>
             </AppBar>
         </Box>
