27f1fcad5a4b994f8d3a007afb850707e3679ac5
stefano <stef.pro@hotmail.com>
2024-01-21 21:27:13 -0500

    chore: create a file for routes

---
+++ src/routes/path.ts
@@ -0,0 +1,9 @@
+export const PATH = {
+    HOME: '/',
+    ABOUT: '/about',
+    CONTACT: '/contact',
+    FORGOTPASSWORD: '/forgotPassword',
+    LOGIN: '/login',
+    REGISTER: '/register',
+    FAVORITES: '/favorites',
+}
