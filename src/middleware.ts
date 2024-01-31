20d5009b89de3a5bff4031bf8d135745fc73e660
stefano <stef.pro@hotmail.com>
2024-01-30 20:38:32 -0500

    chore: setup middleware logic to protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PATH } from "./routes/path";
 
 export function middleware(req: NextRequest) {
-    console.log('mw ran');
+    const cookie = req.cookies.get('signedIn');
+    const baseUrl = 'http://localhost:3000';
+    const url = req.url;
+
+    // Protect the FAVORITES route
+    if (cookie === undefined && url.includes(PATH.FAVORITES)) {
+        return NextResponse.redirect(`${baseUrl}${PATH.HOME}`);
+    }
+
+    // Protect the LOGIN route
+    if (cookie !== undefined && url.includes(PATH.LOGIN)) {
+        return NextResponse.redirect(`${baseUrl}${PATH.HOME}`);
+    }
+
+    // Protect the REGISTER route
+    if (cookie !== undefined && url.includes(PATH.REGISTER)) {
+        return NextResponse.redirect(`${baseUrl}${PATH.HOME}`);
+    }
+
+    // Protect the FORGOT PASSWORD route
+    if (cookie !== undefined && url.includes(PATH.FORGOTPASSWORD)) {
+        return NextResponse.redirect(`${baseUrl}${PATH.HOME}`);
+    }
+
     return NextResponse.next();
 }
