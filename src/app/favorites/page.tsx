84709b57b04419544d3be2f1cde4e87f7cdd4b9f
stefano <stef.pro@hotmail.com>
2024-02-07 20:40:25 -0500

    feat: setup logic to get convert favorites strings to guitars

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,25 +1,38 @@
 'use client';
 import { useAuthContext } from "@/auth/useAuthContext";
 import GeneralContainer from "@/components/generalContainer"
-import { Button } from "@mui/material"
-import { useState } from "react";
+import { Button, Typography } from "@mui/material"
+import { useEffect, useState } from "react";
 import { Guitar } from "../../../types/guitar";
 import GuitarAPI from "../../../helpers/guitar_api_functions";
 
 const Favorites = () => {
     const { fetchFirebaseFavorites } = useAuthContext();
-    const favoritesArray = fetchFirebaseFavorites();
 
     const [favorites, setFavorites] = useState<Guitar[]>([]);
 
-    const handleTest = async () => {
-        const guitar = await GuitarAPI.fetchById("655440104c50c58fe07ea094");
-        console.log(guitar);
-
+    const handleTest = () => {
+        console.log(favorites);
     }
+
+    useEffect(() => {
+        async function fetchFavorites() {
+            const favoritesArray = await fetchFirebaseFavorites();
+            const promises: Promise<Guitar>[] = favoritesArray.map((id: string) => GuitarAPI.fetchById(id));
+            Promise.all(promises)
+                .then((results) => {
+                    setFavorites(results);
+                })
+                .catch((error) => {
+                    console.log("Error fetching guitars: ", error);
+                });
+        }
+        fetchFavorites();
+    }, []);
 
     return (
         <GeneralContainer>
+            <Typography>Your Favorites</Typography>
             <Button onClick={handleTest}>Click here</Button>
         </GeneralContainer>
     );
