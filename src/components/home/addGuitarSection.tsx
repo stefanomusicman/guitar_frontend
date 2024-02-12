8148dd0d7be15240260d1fc1ab8d65634a5798bf
stefano <stef.pro@hotmail.com>
2024-02-12 10:18:53 -0500

    feat: add first section of add guitar form

--- src/components/home/addGuitarSection.tsx
+++ src/components/home/addGuitarSection.tsx
@@ -1,12 +1,34 @@
-import { Typography } from "@mui/material";
+import { Grid, TextField, Typography } from "@mui/material";
 import MainContainer from "./mainContainer";
 import { makeStyles } from "@mui/styles";
+import FormSectionContainer from "./formSectionContainer";
 
 const useStyles = makeStyles(() => ({
     headline: {
         fontFamily: 'Montserrat, sans-serif',
         fontSize: '1.5rem',
         marginBottom: '1em',
+    },
+    formColumn: {
+        width: '85%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'start',
+        alignItems: 'start',
+    },
+    formRow: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    formSectionTitle: {
+        fontFamily: 'Montserrat, sans-serif',
+        fontSize: '1.3rem',
+        marginBottom: '1em',
+    },
+    textField: {
+
     }
 }));
 
@@ -15,7 +37,36 @@ const AddGuitarSection = () => {
 
     return (
         <MainContainer>
-            <Typography className={classes.headline}>Add a Guitar to our Directory!</Typography>
+            <Typography variant="h6" className={classes.headline}>Add a Guitar to our Directory!</Typography>
+            <FormSectionContainer title="Main Information">
+                <TextField
+                    // value={searchTerm}
+                    className={classes.textField}
+                    // onChange={(e) => setSearchTerm(e.target.value)}
+                    id="year"
+                    label="Enter year"
+                    variant="outlined"
+                    InputProps={{ sx: { borderRadius: '10px' } }}
+                />
+                <TextField
+                    // value={searchTerm}
+                    className={classes.textField}
+                    // onChange={(e) => setSearchTerm(e.target.value)}
+                    id="brand"
+                    label="Enter a brand"
+                    variant="outlined"
+                    InputProps={{ sx: { borderRadius: '10px' } }}
+                />
+                <TextField
+                    // value={searchTerm}
+                    className={classes.textField}
+                    // onChange={(e) => setSearchTerm(e.target.value)}
+                    id="model"
+                    label="Enter a model"
+                    variant="outlined"
+                    InputProps={{ sx: { borderRadius: '10px' } }}
+                />
+            </FormSectionContainer>
         </MainContainer>
     );
 }
---
+++ src/components/home/formSectionContainer.tsx
@@ -0,0 +1,44 @@
+import { Grid, Typography } from "@mui/material"
+import { makeStyles } from "@mui/styles"
+import { ReactNode } from "react";
+
+const useStyles = makeStyles(() => ({
+    formColumn: {
+        width: '85%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'start',
+        alignItems: 'start',
+    },
+    formRow: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    formSectionTitle: {
+        fontFamily: 'Montserrat, sans-serif',
+        fontSize: '1.3rem',
+        marginBottom: '1em',
+    },
+}));
+
+type FormSectionContainerProps = {
+    children: ReactNode;
+    title: string;
+}
+
+const FormSectionContainer: React.FC<FormSectionContainerProps> = ({ children, title }) => {
+    const classes = useStyles();
+
+    return (
+        <Grid className={classes.formColumn}>
+            <Typography className={classes.formSectionTitle}>{title}</Typography>
+            <Grid className={classes.formRow}>
+                {children}
+            </Grid>
+        </Grid>
+    )
+}
+
+export default FormSectionContainer;
