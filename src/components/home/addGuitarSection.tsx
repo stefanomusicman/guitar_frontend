84d05f63dbcec22e54636010d07f12ab2a8701c3
stefano <stef.pro@hotmail.com>
2024-02-12 14:52:59 -0500

    chore: create state variables and functions to enable relocation of form fields

--- src/components/home/addGuitarSection.tsx
+++ src/components/home/addGuitarSection.tsx
@@ -3,6 +3,7 @@ import MainContainer from "./mainContainer";
 import { makeStyles } from "@mui/styles";
 import FormSectionContainer from "./formSectionContainer";
 import Colors from "@/app/colors";
+import { useState } from "react";
 
 const useStyles = makeStyles(() => ({
     headline: {
@@ -36,6 +37,33 @@ const useStyles = makeStyles(() => ({
 
 const AddGuitarSection = () => {
     const classes = useStyles();
+
+    // STATE VARIABLES FOR FOR FORM FIELDS
+    // MAIN INFORMATION
+    const [year, setYear] = useState<Number>(0);
+    const [brand, setBrand] = useState<String>('');
+    const [model, setModel] = useState<String>('');
+    // HARDWARE/FRET INFORMATION
+    const [numFrets, setNumFrets] = useState<Number>(0);
+    const [stainlessFrets, setStainlessFrets] = useState<String>('');
+    const [lockingTuners, setLockingTuners] = useState<String>('');
+    // WOOD INFORMATION
+    const [bodyWood, setBodyWood] = useState<String>('');
+    const [neckWood, setNeckWood] = useState<String>('');
+    const [fretboardWood, setFretboardWood] = useState<String>('');
+
+    const [formSubmitted, setFormSubmitted] = useState<Boolean>(false);
+
+    // FUNCTIONS TO PASS INTO INDIVIDUAL COMPONENTS
+    const handleSetYear = (year: Number) => setYear(year);
+    const handleSetBrand = (brand: String) => setBrand(brand);
+    const handleSetModel = (model: String) => setModel(model);
+    const handleSetNumFrets = (num: Number) => setNumFrets(num);
+    const handleSetSSFrets = (input: String) => setStainlessFrets(input);
+    const handleSetTuners = (input: String) => setLockingTuners(input);
+    const handleSetBodyWood = (wood: String) => setBodyWood(wood);
+    const handleSetNeckWood = (wood: String) => setNeckWood(wood);
+    const handleSetFretWood = (wood: String) => setFretboardWood(wood);
 
     const autoCompleteOptions = ['True', 'False'];
     const woodOptions = ['Maple', 'Rosewood', 'Cedar', 'Basswood', 'Ebony', 'Cocobolo', 'Mahogany', 'Alder', 'Bubinga'];
