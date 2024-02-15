eaac90b0616bc9f54d21dbd9392510957c36bc73
stefano <stef.pro@hotmail.com>
2024-02-14 21:07:59 -0500

    chore: create helper class containing methods to carry out form submission

--- helpers/add_guitar_validation.ts
+++ helpers/add_guitar_validation.ts
@@ -2,17 +2,39 @@
 
 -- THINGS THAT NEED TO BE VERIFIED IN ORDER FOR FORM SUBMISSION TO BE SUCCESSFUL
 
-    1. ALL FIELDS MUST BE FILLED....NO EMPTY FIELDS
-    2. YEAR MUST BE A NUMBER
-    3. YEAR MUST BE A NUMBER AFTER 1920
-
+    1. Make sure the following Number fields are not equal to 0
+        - Num Frets
+        - Year
+    2. Make sure the following are not equal to ''
+        - Stainless Steel Frets
+        - Locking Tuners
+        - Body
+        - Neck
+        - Fretboard
+    3. Convert String values to boolean values for the following fields
+        - Stainless Steel Frets
+        - Locking Tuners
 */
 
 class AddGuitarValidation {
-    // VALIDATION OF EMPTY FORM FIELDS
-    static verifyEmptyFormField(input: any): Boolean {
+    // VALIDATION OF EMPTY **NUMBER** FIELDS
+    static verifyEmptyNumberFields(input: Number): Boolean {
+        return input === 0;
+    }
+
+    // VALIDATION OF EMPTY FORM **STRING** FIELDS
+    static verifyEmptyStringField(input: any): Boolean {
         return String(input).trim().length === 0;
     }
 
-    // VALIDATION OF 
+    // CONVERT STRING VALUES TO BOOLEAN VALUES
+    static convertStringToBool(input: String): Boolean {
+        return input === "True";
+    }
+
+    // CREATE METHOD THAT WILL HANDLE ALL CHECKS AND THROW APPROPRIATE ERRORS
+    // ACCEPTANCE CRITERIAS ARE NOT MET
+    static handleFormSubmission(): void {
+
+    }
 }
