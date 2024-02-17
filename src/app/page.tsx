35077d6ddeb86a32e70029855f82bf2187e11339
stefano <stef.pro@hotmail.com>
2024-02-17 18:02:28 -0500

    feat: apply smooth scroll to buttons on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import HeadlineGrid from "@/components/home/headlineGrid";
 import BrandGrid from "@/components/home/brandGrid";
 import SearchSection from "@/components/home/searchSection";
 import AddGuitarSection from "@/components/home/AddGuitarForm/addGuitarSection";
+import React, { useRef } from "react";
 
 const useStyles = makeStyles(() => ({
   mainContainer: {
@@ -46,10 +47,27 @@ const useStyles = makeStyles(() => ({
 export default function Home() {
   const classes = useStyles();
 
+  const searchSectionRef = useRef<HTMLDivElement>(null);
+  const addGuitarSectionRef = useRef<HTMLDivElement>(null);
+
   const theme = useTheme();
   const isMobile: Boolean = useMediaQuery(theme.breakpoints.down('md'));
 
   const fontSize: string = isMobile ? '2rem' : '2.5rem';
+
+  // Function to scroll to the search section
+  const scrollToSearchSection = () => {
+    if (searchSectionRef.current) {
+      searchSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  // Function to scroll to the add guitar section
+  const scrollToAddGuitarSection = () => {
+    if (addGuitarSectionRef.current) {
+      addGuitarSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <Box className={classes.mainContainer}>
@@ -60,13 +78,17 @@ export default function Home() {
       </Box>
       {/* Action Buttons */}
       <Box sx={{ width: isMobile ? '75%' : '35%' }} className={classes.actionButtonContainer}>
-        <Button disableElevation sx={{ backgroundColor: Colors.primaryBlue }} className={classes.button} variant="contained">Search</Button>
-        <Button disableElevation sx={{ backgroundColor: Colors.primaryOrange }} className={classes.button} variant="contained">Add</Button>
+        <Button onClick={scrollToSearchSection} disableElevation sx={{ backgroundColor: Colors.primaryBlue }} className={classes.button} variant="contained">Search</Button>
+        <Button onClick={scrollToAddGuitarSection} disableElevation sx={{ backgroundColor: Colors.primaryOrange }} className={classes.button} variant="contained">Add</Button>
       </Box>
       <HeadlineGrid />
       <BrandGrid />
-      <SearchSection />
-      <AddGuitarSection />
+      <Box style={{ width: '100%' }} ref={searchSectionRef}>
+        <SearchSection />
+      </Box>
+      <Box style={{ width: '100%' }} ref={addGuitarSectionRef}>
+        <AddGuitarSection />
+      </Box>
     </Box>
   )
 }
