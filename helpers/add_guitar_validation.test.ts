c84a4193c545432ed492402f63c5696d8e609add
stefano <stef.pro@hotmail.com>
2024-02-24 08:17:20 -0500

    chore: write tests for verifyEmptyStringField function

--- helpers/add_guitar_validation.test.ts
+++ helpers/add_guitar_validation.test.ts
@@ -1,5 +1,6 @@
 import AddGuitarValidation from "./add_guitar_validation"
 
+// Tests for verifyEmptyNumberFields function
 describe('check number is not 0', () => {
     test('0 equals to true', () => {
         expect(AddGuitarValidation.verifyEmptyNumberFields(0)).toBe(true);
@@ -7,5 +8,20 @@ describe('check number is not 0', () => {
 
     test('2 equals to false', () => {
         expect(AddGuitarValidation.verifyEmptyNumberFields(2)).toBe(false);
+    });
+});
+
+// Test for verifyEmptyStringField function
+describe('check that string is not empty', () => {
+    test('empty string equals to true', () => {
+        expect(AddGuitarValidation.verifyEmptyStringField('')).toBe(true);
+    });
+
+    test('empty string with white space equals to true', () => {
+        expect(AddGuitarValidation.verifyEmptyStringField('  ')).toBe(true);
+    });
+
+    test('not empty string equals to false', () => {
+        expect(AddGuitarValidation.verifyEmptyStringField('hello')).toBe(false);
     });
 })
