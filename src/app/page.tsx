a40d38b761b54d8772dbe982b550138befd397a9
stefano <stef.pro@hotmail.com>
2024-02-29 08:18:35 -0500

    chore: revert useEffect in home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-'use client';
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 import { Metadata } from "next";
 import HomeContent from "@/components/home/homeContent";
-import { FIREBASE_API } from "@/config-global";
 
-// export const metadata: Metadata = {
-//   title: 'Home',
-//   description: 'This is the main page of the site'
-// }
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'This is the main page of the site'
+}
 
 export default function Home() {
-  useEffect(() => {
-    console.log('This is the FIREBASE information');
-    console.log(FIREBASE_API);
-  }, [])
-
   return (
     <Fragment>
       <HomeContent />
